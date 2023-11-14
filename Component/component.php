75c3bee743afbcab87fab1d9ce35a124a51bae5c
WessimH <wessim.harmel1@gmail.com>
2023-11-14 09:38:44 +0100

    ss

---
+++ Component/component.php
@@ -0,0 +1,28 @@
+<?php
+class component{
+    function PosteUtulisateur(){}
+}
+function HautDePage()
+{
+    return ?>
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo str_replace($_SERVER['PHP_SELF'], ".php") ?></title>
+        <script src="js/modeSelector.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+              integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+              crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body> <?php ;
+}
+function BasDePage()
+{
+    return ?>
+    </body>
+    </html> <?php ;
+}
+
+
